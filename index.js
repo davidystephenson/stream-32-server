507a1b825628860ef9167148ad4e03f84800beb1
David Y. Stephenson <david@davidystephenson.com>
2019-11-19 11:28:24 +0100

    Set up Room model

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express()
 
 const stream = new Sse()
 
-app.get('/stream', (request, response) => {
-  stream.init(request, response)
-})
+app.get(
+  '/stream',
+  (request, response) => {
+    stream.init(request, response)
+  }
+)
 
 const port = 4000
 
