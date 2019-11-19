f9c42ce4be4cddaa10ddac0e1fc0ed501b1613ee
David Y. Stephenson <david@davidystephenson.com>
2019-11-19 15:10:42 +0100

    Relate users to rooms

--- room/model.js
+++ room/model.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
+const User = require('../user/model')
+
 const Room = db.define('room', {
   name: Sequelize.STRING
 })
+
+User.belongsTo(Room)
 
 module.exports = Room
